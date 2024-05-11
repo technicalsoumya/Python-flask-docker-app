928bdc2ce69d06abbb9a7693a744c19bde8600ea
technicalsoumya <140595074+technicalsoumya@users.noreply.github.com>
2024-05-11 14:06:54 +0530

    2nd commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Python Flask Application - Ashok IT"
+    return "Welcome to city of joy kolkata"
 
 
 if __name__ == "__main__":
